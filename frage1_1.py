e21959be8a97d293b0241674983b4aec967f39e4
ClubMateEnjoyer <maxbehne9@gmail.com>
2025-01-06 23:15:32 +0100

    Add files via upload
    -mehr kommentare
    -bisschen struktut

--- frage1_1.py
+++ frage1_1.py
@@ -1,30 +1,32 @@
 import psycopg2
 import matplotlib.pyplot as plt
 
-DB_HOST = "db_host"
-DB_PORT = 1234
-DB_NAME = "db_name"
-DB_USER = "db_user"
-DB_PASS = "db_pass"
+#Datenbankattribute
+host = "localhost"
+port = 5432
+name = "aol_data"
+user = "postgres"
+pwd = "password"
 
+#connection zur DB herstellen
 try:
-    connection = psycopg2.connect(
-        host = DB_HOST,
-        port = DB_PORT,
-        database = DB_NAME,
-        user = DB_USER,
-        password = DB_PASS
+    conn = psycopg2.connect(
+        host = host,
+        port = port,
+        database = name,
+        user = user,
+        password = pwd
     )
     print("Verbindung erfolgreich :)")
 except Exception as e:
-    print("Fehler beim Verbinden: ", e)
+    print("Fehler beim verbinden: ", e)
 
-cursor = connection.cursor()
+#cursor erstellen um sql-statements auszuführen
+cur = conn.cursor()
 
-# sql_queries
+#Frage 1_1: Suchanfragen nach Uhrzeit
 
-#Frage 1
-sql_query1 = """
+sql_query = """
 SELECT 
     EXTRACT(HOUR FROM "Uhrzeit") AS hour_of_day, 
     COUNT(*) AS entry_count
@@ -35,23 +37,29 @@ WHERE
 GROUP BY 
     EXTRACT(HOUR FROM "Uhrzeit")
 ORDER BY 
-    entry_count DESC;
+    entry_count DESC
+LIMIT 100;
 """
 
-cursor.execute(sql_query1)
+#Query ausführen
+cur.execute(sql_query)
 
-result = cursor.fetchall()
-for row in result:
-    print(row)
+#Ergebnisse speichern
+result = cur.fetchall()
+#print(result)
 
-kategorie, werte = zip(*result)
+#2 listen erstellen, liste 1: Uhrzeiten, liste 2: anzahl suchanfragen
+uhrzeit, suchanfragen = zip(*result)
+#print(uhrzeit)
+#print(suchanfragen)
 
-
-plt.bar(kategorie, werte, color='skyblue')
+#Diagramme erstellen
+plt.bar(uhrzeit, suchanfragen, color="#ff6b4d")
 plt.xlabel('Uhrzeit')
 plt.ylabel('Anzahl Suchanfragen')
-plt.title('Suchanfragen nach Uhrzeit')
+plt.title('Suchanfragen nach Uhrzeiten')
 plt.show()
 
-cursor.close()
-connection.close()
+#Resourcen wieder schließen wenn fertig
+cur.close()
+conn.close()
